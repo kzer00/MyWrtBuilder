ac7c73b5f7fe3b36943022939bb1c74ec7fa1ee6
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-01-25 02:39:23 +0700

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -4,15 +4,6 @@ echo "Start Builder Patch !"
 echo "Current Path: $PWD"
 
 cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-rockchip-armv8.Linux-x86_64 || exit
-
-# Remove redundant default packages
-sed -i "/luci-app-cpufreq/d" include/target.mk
-
-# Force opkg to overwrite files
-sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
-
-# Not generate ISO images for it is too big
-sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
-
-# Not generate VHDX images
-sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
+     echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic " >> repositories.conf
+     echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/aarch64_generic" >> repositories.conf
+     sed -i 's/option check_signature/# option check_signature/g' repositories.conf
