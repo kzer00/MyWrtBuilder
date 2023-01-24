bbfb37e4a42f2025280b828ffe4a325c2de831c1
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-01-25 02:08:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,95 +4,38 @@
 make info
 
 # 主配置名称
-PROFILE="generic"
+PROFILE="xunlong_orangepi-r1-plus-lts"
 
-PACKAGES=""
-
-# Argon 主题
-PACKAGES="$PACKAGES luci-theme-argon luci-i18n-argon-config-zh-cn"
-
-# 常用系统管理组件
-
-# Diskman 磁盘管理
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-# CPU 占用限制
-PACKAGES="$PACKAGES luci-i18n-cpulimit-zh-cn"
-# collectd 统计
-PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
-
-# 常用软件服务
-
-# OpenClash 代理
-PACKAGES="$PACKAGES luci-app-openclash"
-# Passwall 代理
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-# frp 和 nps 内网穿透
-PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
-
-# 常用的网络存储组件
-
-# 文件助手
-PACKAGES="$PACKAGES luci-app-fileassistant"
-# 硬盘休眠
-PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
-# p910nd 打印服务器
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
-# GoWebDav
-PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
-# Samba 网络共享
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# qBittorrent 下载器
-PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
-# Aria2 下载器
-PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
-# FTP 服务器
-PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
-
-# Docker 组件
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn"
-
-# VPN 类
-
-# IPSec VPN
-PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
-# Zerotier
-PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
-
-# 网络组件
-
-# EQoS 网速控制
-PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
-# 多线多拨与负载均衡
-PACKAGES="$PACKAGES luci-app-syncdial luci-i18n-mwan3-zh-cn"
-# 宽带监控 Nlbwmon
-PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
-
-# 界面翻译补全
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-turboacc-zh-cn"
-
-# Packages 文件夹下的 ipk 包
-PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
-
-# 一些自己需要的内核组件
-PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
-
-# 一些其他可能有用的包
-
-# zsh 终端
-PACKAGES="$PACKAGES zsh"
-# Vim 完整版，带语法高亮
-PACKAGES="$PACKAGES vim-fuller"
-# X/Y/ZMODEM 文件传输
-PACKAGES="$PACKAGES lrzsz"
-# OpenSSH
-PACKAGES="$PACKAGES openssh-server openssh-client"
-# Netdata 系统监控界面
-PACKAGES="$PACKAGES netdata"
+PACKAGES="acpid attr base-files bash bc bind-server blkid block-mount blockd bsdtar  \
+        btrfs-progs busybox bzip2 cgi-io chattr comgt comgt-ncm coremark  \
+        coreutils coreutils-base64 coreutils-nohup coreutils-truncate curl \
+        dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs  \
+        f2fs-tools f2fsck fdisk gawk getopt gzip hostapd-common iconv iw iwinfo jq jshn  \
+        libjson-script \
+        liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs  \
+        mount-utils openssl-util parted perl-http-date perlbase-file perlbase-getopt  \
+        perlbase-time perlbase-unicode perlbase-utf8 pigz ppp  \
+        proto-bonding pv rename resize2fs runc subversion-client subversion-libs tar  \
+        tini ttyd tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch  \
+        uuidgen wget-ssl whereis which wwan xfs-fsck xfs-mkfs xz  \
+        xz-utils ziptool zoneinfo-asia zoneinfo-core zstd  \
+        \
+        luci luci-base luci-compat luci-i18n-base-en luci-lib-base  \
+        luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
+        luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
+        luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
+        luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
+        atinout modeminfo-serial-xmm kmod-usb-net-rndis \
+        dnsmasq-full \
+        openssh-sftp-server modeminfo-serial-fibocom \
+        luci-app-modeminfo xmm-modem modeminfo \
+        luci-app-atinout luci-app-openclash "
 
 # 一些自定义文件
-FILES="files"
+#FILES="files"
 
 # 禁用 openssh-server 的 sshd 服务和 docker 的 dockerd 服务以防止冲突
-DISABLED_SERVICES="sshd dockerd"
+#DISABLED_SERVICES="sshd dockerd"
 
-make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
+#make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
+make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
