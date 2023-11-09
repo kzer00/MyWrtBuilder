52838035e119eba9ed44a59cee67bd667c7312cb
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-09 23:22:59 +0700

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -4,6 +4,6 @@ echo "Start Builder Patch !"
 echo "Current Path: $PWD"
 
 cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-rockchip-armv8.Linux-x86_64 || exit
-     echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic " >> repositories.conf
-     echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/aarch64_generic" >> repositories.conf
+     #echo "src/gz custom_generic https://raw.githubusercontent.com/indowrt/indowrt/main/generic " >> repositories.conf
+     echo "src/gz custom_arch https://raw.githubusercontent.com/indowrt/indowrt/main/aarch64_generic" >> repositories.conf
      sed -i 's/option check_signature/# option check_signature/g' repositories.conf
