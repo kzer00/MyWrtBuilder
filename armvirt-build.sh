b5539a59053b2ac7bb51ee7a76df9f818d9d5999
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-16 10:04:29 +0700

    Update armvirt-build.sh

--- armvirt-build.sh
+++ armvirt-build.sh
@@ -4,14 +4,26 @@
 make info
 
 # 主配置名称
-PROFILE="Generic"
+PROFILE="Generic EFI Boot"
 
-PACKAGES="usb-modeswitch kmod-usb-net-asix-ax88179 kmod-usb-net-asix \
+PACKAGES="base-files ca-bundle dropbear fstools libc libgcc libustream-mbedtls logd mtd netifd opkg uci \
+         uclient-fetch urandom-seed urngd busybox procd procd-ujail procd-seccomp mkf2fs e2fsprogs blkid -dnsmasq \
+         firewall4 nftables kmod-nft-offload odhcp6c odhcpd-ipv6only ppp ppp-mod-pppoe \
+         kmod-amazon-ena kmod-e1000e kmod-vmxnet3 kmod-rtc-rx8025 kmod-i2c-mux-pca954x kmod-gpio-pca953x partx-utils \
+         kmod-wdt-sp805 kmod-mvneta kmod-mvpp2 kmod-fsl-dpaa1-net kmod-fsl-dpaa2-net kmod-fsl-enetc-net kmod-dwmac-imx kmod-fsl-fec kmod-thunderx-net \
+         kmod-dwmac-rockchip kmod-dwmac-sun8i kmod-phy-aquantia \
+         kmod-phy-broadcom kmod-phy-marvell kmod-phy-marvell-10g kmod-sfp kmod-atlantic kmod-bcmgenet kmod-octeontx2-net \
+
+       
+       
+       
+       
+        usb-modeswitch kmod-usb-net-asix-ax88179 kmod-usb-net-asix \
         luci-compat luci-proto-wireguard kmod-usb-net-huawei-cdc-ncm \
         luci-proto-3g luci-proto-ncm  \
         luci-proto-qmi usb-modeswitch \
         atinout modeminfo-serial-xmm kmod-usb-net-rndis \
-        dnsmasq-full -dnsmasq htop \
+        dnsmasq-full htop \
         openssh-sftp-server modeminfo-serial-fibocom luci-theme-material \
         luci-app-modeminfo xmm-modem modeminfo luci-app-ttyd \
         luci-app-atinout-mod luci-app-openclash "
