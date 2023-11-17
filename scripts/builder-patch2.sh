bdc2cd237e993e292f7323d7a9fb3cb5dfa720a3
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-18 02:48:29 +0700

    Update builder-patch2.sh

--- scripts/builder-patch2.sh
+++ scripts/builder-patch2.sh
@@ -3,7 +3,7 @@
 echo "Start Builder Patch !"
 echo "Current Path: $PWD"
 
-cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-armsr-armv8.Linux-x86_64 || exit
+cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-armsr-armv8.Linux-x86_64 
      #echo "src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic " >> repositories.conf
      #echo "src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/aarch64_cortex-a53" >> repositories.conf
      #sed -i 's/option check_signature/# option check_signature/g' repositories.conf
