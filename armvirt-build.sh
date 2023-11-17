a85a43e63c22eb3e4b9d3a11dbdd6f94f4791771
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-18 02:33:04 +0700

    Update armvirt-build.sh

--- armvirt-build.sh
+++ armvirt-build.sh
@@ -7,7 +7,15 @@ make info
 PROFILE="generic"
 
 PACKAGES="
-        -dnsmasq dnsmasq-full luci-app-openclash "
+        hostapd-common iw iwinfo \
+        kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 wpa-cli wpad-basic \
+        luci luci-compat luci-lib-ipkg luci-theme-material \
+        luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
+        luci-proto-3g luci-proto-ncm  \
+        luci-proto-wireguard luci-proto-qmi usb-modeswitch luci-app-ttyd \
+        kmod-usb-net-rndis -dnsmasq dnsmasq-full \
+        openssh-sftp-server luci-app-openclash luci-app-internet-detector\
+        luci-theme-neobirdkawe xmm-modem luci-app-modeminfo luci-app-atinout-mod \ "
 
 
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
