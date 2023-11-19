9e9cefe47675db0a9948b40e03fa67af8faf9dd4
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-19 22:52:25 +0700

    Update bahan.sh

--- bahan.sh
+++ bahan.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+# 打印 info
+make info
+
+# 主配置名称
+PROFILE="xunlong_orangepi_zero_plus"
+
+PACKAGES="
+        luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
+        luci-proto-openconnect luci-proto-ppp luci-proto-qmi   \
+        atinout modeminfo-serial-xmm kmod-usb-net-rndis \
+        -dnsmasq dnsmasq-full \
+        openssh-sftp-server modeminfo-serial-fibocom luci-theme-material \
+        luci-app-modeminfo xmm-modem modeminfo \
+        luci-app-atinout-mod luci-app-openclash "
+
+# 一些自定义文件
+#FILES="files"
+
+# 禁用 openssh-server 的 sshd 服务和 docker 的 dockerd 服务以防止冲突
+#DISABLED_SERVICES="sshd dockerd"
+
+#make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
+make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
