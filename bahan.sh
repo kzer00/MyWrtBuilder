5cb72780983f9b278112c98a13917b75c5b7d456
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-22 03:35:05 +0700

    Update bahan.sh

--- bahan.sh
+++ bahan.sh
@@ -7,21 +7,21 @@ make info
 PROFILE="xunlong_orangepi-zero-plus"
 
 PACKAGES="
-        hostapd-common iw iwinfo \
+        hostapd-common iw iwinfo luci-app-tinyfm \
         kmod-cfg80211 kmod-mac80211 wpa-cli wpad-basic \
         luci luci-compat luci-lib-ipkg luci-theme-material \
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-3g luci-proto-ncm usbutils luci-compat \
         luci-proto-wireguard luci-proto-qmi usb-modeswitch luci-app-ttyd \
         kmod-usb-net-rndis -dnsmasq dnsmasq-full \
-        openssh-sftp-server luci-app-openclash luci-app-openclash\
+        openssh-sftp-server luci-proto-xmm luci-app-openclash luci-app-openclash\
         luci-theme-neobirdkawe xmm-modem luci-app-modeminfo "
 
 # 一些自定义文件
-#FILES="files"
+FILES="files"
 
 # 禁用 openssh-server 的 sshd 服务和 docker 的 dockerd 服务以防止冲突
 #DISABLED_SERVICES="sshd dockerd"
 
 #make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
-make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
+make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES"
