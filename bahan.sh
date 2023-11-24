2f09565d2fd5c765a652188320461507abcf1413
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-24 12:37:35 +0700

    Update bahan.sh

--- bahan.sh
+++ bahan.sh
@@ -6,15 +6,15 @@ make info
 # 主配置名称
 PROFILE="xunlong_orangepi-zero-plus"
 
-PACKAGES="
+PACKAGES="\
         hostapd-common iw iwinfo modeminfo-serial-xmm modeminfo-serial-fibocom \
-        kmod-cfg80211 kmod-mac80211 wpa-cli wpad-basic kmod-usb-net-huawei-cdc-ncm\
+        kmod-cfg80211 kmod-mac80211 wpa-cli wpad-basic kmod-usb-net-huawei-cdc-ncm \
         luci luci-compat luci-lib-ipkg luci-theme-material usbutils\
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-3g luci-proto-ncm usbutils luci-compat usb-modeswitch\
         luci-proto-wireguard luci-proto-qmi usb-modeswitch luci-app-ttyd \
         kmod-usb-net-rndis -dnsmasq dnsmasq-full luci-theme-material\
-        openssh-sftp-server luci-proto-xmm luci-app-openclash luci-app-openclash\
+        openssh-sftp-server luci-proto-xmm luci-app-openclash luci-app-openclash \
         luci-theme-neobirdkawe xmm-modem luci-app-modeminfo "
 
 # 一些自定义文件
